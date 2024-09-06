eb2744b86897f4a9bf98ad537f4d460fd8c42274
vlad05x <organizm777alf@gmail.com>
2024-09-06 11:52:36 +0300

    Added js file

---
+++ index.js
@@ -0,0 +1,28 @@
+// To get the message
+document
+  .getElementById("contactForm")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+
+    alert(
+      `Ваше ім'я: ${name}\nВаш email: ${email}\nПовідомлення успішно відправлено`
+    );
+  });
+
+// For authorization.
+function openTab(evt, tabName) {
+  let tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (let i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (let i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(tabName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
